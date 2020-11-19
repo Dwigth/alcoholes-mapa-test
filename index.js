b9d00f6556840a5d504530eac941787a9dc0f5a7
fabian2190 <fabian_2190@hotmail.com>
2020-11-19 13:28:18 -0600

    Latitud Longitud

--- index.js
+++ index.js
@@ -7,9 +7,11 @@
  * licencia
  * domicilio:{municipio}{localidad}{colonia}
  * 
- * @17.984515,-92.9327628,
- * rula: 01545REMP-(-45.4554744,95.65595949) -OTROCODIGO
- * 01545REOMPIP4545547449565595949OfpTROCODIGO
+ * @17.549286, -92.947944
+ * 
+ * rula: 4 Primeros Digitos RFC - Licencia  10 Caracteres - Giro (2Letras) - LATITUD - LONGITUD
+																EXMPLE:	LA P 17 D 549286  LO M 92 D 947944 EC = 17.549286, -92.947944
+ * IUAA0000LA2500MSLAP17D549286LOM92D947944EC12345
  * 
  * @param rawParams {string} Obtiene los parametros enviados atraves de la url.
  * @param coord     {array}  Tupla que contiene la latitud y la longitud. [lat,long].
@@ -19,23 +21,50 @@ class RULA {
     constructor() {
         this.coord = [0, 0];
         this.obj = new Map();
-        this.rawParams = window.location.search.substr(1);
+        this.rawParams = window.location.search.substr(1);  
+        var data;
         if (this.rawParams.length == 0) {
             alert('No se han encontrado parametros en la URL.');
             return;
         }
+        
         this.FormatParams();
         this.DrawTable();
         this.GetLatLong();
-        // this.DrawMap();
+        this.DrawMap();
     }
 
-    GetLatLong() {
-        // code
+    GetLatLong() {        
+        var rula = this.obj.get("rula")
+        let temp = rula.slice(16, rula.length); 
+        temp = temp.slice(2, 26);       
+        let cordenadas = temp.split("LO");       
+        let latitud = cordenadas[0];
+        let longitud = cordenadas [1];       
+
+        if (longitud.indexOf('EC')!= -1)
+        {
+            longitud = longitud.replace('EC', '');
+        }        
+                
+        //Formatear 
+        longitud = longitud.replace('P', '');
+        longitud = longitud.replace('M', '-');
+        longitud = longitud.replace('D', '.');
+
+        latitud = latitud.replace('P', '');
+        latitud = latitud.replace('M', '-');
+        latitud = latitud.replace('D', '.');
+
+        this.coord = [latitud, longitud];
     }
 
     FormatParams() {
-        const formatArr = this.rawParams.split('&');
+        var str = this.rawParams;
+        str = decodeURI(str);
+        //const formatArr = this.rawParams.split('&');
+
+        const formatArr = str.split('&');
         formatArr.forEach(param => {
             const result = param.split('=');
             this.obj.set(result[0], result[1]);
@@ -64,7 +93,7 @@ class CustomMap {
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [coord[1], coord[0]],
-            zoom: 8
+            zoom: 18
         });
 
         const marker = new mapboxgl.Marker()
