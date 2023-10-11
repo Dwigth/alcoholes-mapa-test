bc6110f3616e1e2345913e0cf666e72818991057
Dwigth Astacio <dwigth32@outlook.es>
2023-10-11 13:42:41 -0600

    feat(map): - add column classes - change components order - change styles of components

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import googleMapsIcon from "./assets/google-maps-icon.png"
 import { useEffect, useState } from "react";
-import { Coordinates, IFrameConfiguration } from "./types";
+import { IFrameConfiguration } from "./types";
 import { googleMapIframeSource, googleMapUrl } from "./google-maps-urls";
 
 const Map = styled.div`
-width:100%; margin: 0 auto;
+width: 100%;
+margin: 0 auto;
 margin-bottom: 30px;
 `;
 
@@ -14,10 +15,16 @@ width: 70px;
 height:70px;
 `;
 
-const MapHyperlink = styled.a``;
+const MapHyperlink = styled.a`
+color: #007bff;
+text-decoration: none;
+background-color: transparent;
+`;
 
 const GoogleMapIframe = styled.iframe`
-
+width: 100%;
+height: 500px;
+border: 2px solid black;
 `;
 
 function GoogleMap(props: IFrameConfiguration) {
@@ -33,11 +40,11 @@ function GoogleMap(props: IFrameConfiguration) {
 
     return (
         <>
-            <Map>
-                <GoogleMapIframe src={iframeSrc}></ GoogleMapIframe>
+            <Map className="twelve columns">
                 <MapHyperlink href={url}>
-                    <GoogleMapIcon src={googleMapsIcon} alt="imagen" />
+                    <GoogleMapIcon src={googleMapsIcon} alt="icono de google maps" />
                 </MapHyperlink>
+                <GoogleMapIframe src={iframeSrc}></ GoogleMapIframe>
             </Map>
         </>
     )
